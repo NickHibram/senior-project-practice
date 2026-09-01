c8ce599bc9589c9df32bc09d9fff8437f70d6a1e
NickHibram <23nicholas.d.abram@gmail.com>
2026-09-01 13:24:35 -0400

    added I/O to get student information

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
-#name = input("Please enter your name: ")
-name = "Nicholas Abram"
-major = "Computer Science"
-interest = "Artificial Intelligence"
-skill_goal = "Team Collaboration"
-print(f"Senior Project Developer Profile\nName: {name}\nMajor: {major}\nTechnology Interest: {interest}\nSkill Goal: {skill_goal}")
+name = input("Please enter your name: ")
+major = input("Please enter your major: ")
+interest = input("Please enter your technology interest: ")
+skill_goal = input("Please enter your skill goal: ")
+print(f"\n\n\tSenior Project Developer Profile\nName: {name}\nMajor: {major}\nTechnology Interest: {interest}\nSkill Goal: {skill_goal}")

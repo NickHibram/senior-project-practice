604645266aea36c4c8c510935ad5856533884782
NickHibram <23nicholas.d.abram@gmail.com>
2026-09-01 13:20:24 -0400

    Part 4 Commit

---
+++ app.py
@@ -0,0 +1,4 @@
+#name = input("Please enter your name: ")
+name = "Nicholas Abram"
+major = "Computer Science"
+print(f"Senior Project Developer Profile\nName: {name}\nMajor: {major}\nTechnology Interest: Artificial Intelligence\nSkill Goal: Team Collaboration")
